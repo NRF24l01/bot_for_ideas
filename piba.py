2095d715eaa61361f085d5b10c5bb9386281db27
$sudo heck beluga <s.nevrovsky@ya.ru>
2023-01-17 22:11:28 +0300

    Upload

---
+++ piba.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+try:
+    conn = sqlite3.connect("data.db")
+    cursor = conn.cursor()
+
+    txt = cursor.execute("SELECT user_id FROM profiles WHERE user_id='102423' ")
+
+
+    conn.commit()
+
+except sqlite3.Error as error:
+    print("Error: ", error)
+
+finally:
+    if(conn):
+        conn.close()
---
+++ vb b vgfn.py
@@ -0,0 +1,7 @@
+a = 0
+res = 0
+while a <= 173:
+    a = a + 1
+    res = res + a
+    print(str(a)+"        "+str(res))
+print(res/173)
